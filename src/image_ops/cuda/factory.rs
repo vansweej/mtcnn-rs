15fdec3c03a13d359d6622405c59c2f970da6cab
Jan Van Sweevelt <vansweej@gmail.com>
2021-05-06 22:58:30 +0200

    test for factory

--- src/image_ops/cuda/factory.rs
+++ src/image_ops/cuda/factory.rs
@@ -19,18 +19,35 @@ impl ImageOpFactory<CudaImage<u8>, CudaError> for CudaImageOpFactory {}
 #[cfg(test)]
 mod tests {
     use super::*;
-    use rustacuda::prelude::*;
+    use crate::image_ops::*;
+    use crate::img_op;
+    use image::RgbImage;
+    use npp_rs::cuda::initialize_cuda_device;
+    use pretty_assertions::assert_eq;
+    use std::cell::RefCell;
+    use std::convert::TryFrom;
+    use std::rc::Rc;
 
     #[test]
     fn test_cuda_factory() {
-        rustacuda::init(rustacuda::CudaFlags::empty()).unwrap();
-        let device = Device::get_device(0).unwrap();
-        let _ctx =
-            Context::create_and_push(ContextFlags::MAP_HOST | ContextFlags::SCHED_AUTO, device)
-                .unwrap();
+        let _ctx = initialize_cuda_device().unwrap();
 
         let test = CudaImageOpFactory {};
 
-        let op1 = test.create_resize_imageop(1024, 768);
+        let op1 = img_op!(test.create_resize_imageop(1024, 768));
+
+        let img1 = image::open("test_resources/2020-11-21-144033.jpg").unwrap();
+
+        let cuda_src1 = Rc::new(RefCell::new(
+            CudaImage::try_from(img1.as_rgb8().unwrap()).unwrap(),
+        ));
+
+        let res1 = op1(Rc::clone(&cuda_src1)).and_then(cuda::into_inner);
+
+        let result_img1 = res1.map(|c| RgbImage::try_from(&c)).unwrap().unwrap();
+
+        let dim1 = result_img1.dimensions();
+        assert_eq!(dim1.0, 1024);
+        assert_eq!(dim1.1, 768);
     }
 }
